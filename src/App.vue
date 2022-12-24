453fa6464d27e790523741128d836f963523ff1e
a99os <abbosashirboev@gmail.com>
2022-12-24 12:06:19 +0500

    add event

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="comment" v-for="comment in comments" v-bind:key="comment.id">
+  <div class="w-75 mx-auto">
+    <h3>Add comment</h3>
+    <form>
+      <input
+        @input="name = $event.target.value"
+        :value="name"
+        type="text"
+        placeholder="Enter your name"
+        class="form-control"
+      />
+      <input
+        @input="phone = $event.target.value"
+        :value="phone"
+        type="text"
+        placeholder="Enter your phone"
+        class="form-control my-3"
+      />
+      <input
+        @input="content = $event.target.value"
+        :value="content"
+        type="text"
+        placeholder="Enter your content"
+        class="form-control"
+      />
+      <div class="d-flex flex-row-reverse">
+        <button @click="createComment" class="btn btn-primary mt-3">
+          Add Comment
+        </button>
+      </div>
+    </form>
+  </div>
+  <div
+    class="w-75 mx-auto comment"
+    v-for="comment in comments"
+    v-bind:key="comment.id"
+  >
     <div><b>Name:</b>{{ comment.name }}</div>
     <div><b>Phone:</b>{{ comment.phone }}</div>
     <div><b>Content:</b>{{ comment.content }}</div>
@@ -14,14 +49,23 @@ export default {
         { id: 2, name: "Sobir", phone: 32435, content: "Lorem ipsum conten" },
         { id: 3, name: "Qodir", phone: 5425332, content: "Lorem ipsum conten" },
       ],
+      name: "",
+      phone: "",
+      content: "",
     };
   },
   methods: {
-    increment() {
-      this.count++;
-    },
-    decrement() {
-      this.count--;
+    createComment(e) {
+      e.preventDefault();
+      this.comments.push({
+        id: this.comments.length + 1,
+        name: this.name,
+        phone: this.Phone,
+        content: this.content,
+      });
+      this.name = "";
+      this.phone = "";
+      this.content = "";
     },
   },
 };
